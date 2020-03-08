0d90345858c6d9183fa50bbd41decafb7527aedb
Scott Munro <scottnmunro@gmail.com>
2020-03-07 18:36:32 -0800

    add a static build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,7 +4,33 @@
 # TODO: build test exe and link in dll
 # TODO: C11 support - build with clang
 
+Param(
+    [switch] $Direct,
+    [switch] $Static
+    )
+
+# Default to build all if nothing is specified
+$All = (!$Direct -and !$Static);
+
 $SrcDir = "$PSScriptRoot\..\src\"
 $TargetDir = "$PSScriptRoot\..\target\"
 
-cl /TC $SrcDir\demo_lib_test.c $SrcDir\demo_lib.c /Fo:$TargetDir /Fe:$TargetDir\demo_lib_test_direct.exe
+# Direct build
+if ($Direct -or $All)
+{
+    Write-Warning "DIRECT: compile all"
+    cl /TC $SrcDir\demo_lib_test.c $SrcDir\demo_lib.c /Fo:$TargetDir /Fe:$TargetDir\demo_lib_test_direct.exe
+}
+
+# Static build
+if ($Static -or $All)
+{
+    Write-Warning "STATIC: compile libobj"
+    cl /TC $SrcDir\demo_lib.c /c /Fo:$TargetDir\demo_lib_static_lib.obj
+
+    Write-Warning "STATIC: link libobj to lib"
+    lib $TargetDir\demo_lib_static_lib.obj /OUT:$TargetDir\demo_lib_static.lib
+
+    Write-Warning "STATIC: compile test exe"
+    cl /TC $SrcDir\demo_lib_test.c /Fo:$TargetDir /Fe:$TargetDir\demo_lib_test_static.exe /link $TargetDir\demo_lib_static.lib
+}
