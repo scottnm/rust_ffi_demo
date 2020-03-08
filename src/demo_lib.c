3ace5debae4c613a38b26d0815f9392da65ab4f2
Scott Munro <scottnmunro@gmail.com>
2020-03-07 18:01:57 -0800

    add stub impl

--- src/demo_lib.c
+++ src/demo_lib.c
@@ -1,3 +1,57 @@
 #include "demo_lib.h"
 
 #define UNUSED(x) do { (void)(x); } while(0)
+
+int
+GetEvents(
+    _Out_ uint32_t* count,
+    _Out_ DL_Event_List* events
+    )
+{
+    // TODO: impl
+    UNUSED(count);
+    UNUSED(events);
+    return 1;
+}
+
+int
+ReturnEvents(
+    uint32_t count,
+    DL_Event_List events
+    )
+{
+    // TODO: impl
+    UNUSED(count);
+    UNUSED(events);
+    return 1;
+}
+
+int
+HandleCreatedEvent(
+    const char* creationString
+    )
+{
+    // TODO: impl
+    UNUSED(creationString);
+    return 1;
+}
+
+int
+HandleDestroyedEvent(
+    uint8_t destroyedByte
+    )
+{
+    // TODO: impl
+    UNUSED(destroyedByte);
+    return 1;
+}
+
+int
+HandleChangedEvent(
+    ChangedState changedState
+    )
+{
+    // TODO: impl
+    UNUSED(changedState);
+    return 1;
+}
