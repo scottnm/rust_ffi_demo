2dd52ccee78082935a6d98f4111f4b7d7ab533bf
Scott Munro <scottnmunro@gmail.com>
2020-03-07 18:15:56 -0800

    add a quick print to make it clear which test we are running

--- src/demo_lib_test.c
+++ src/demo_lib_test.c
@@ -1,7 +1,14 @@
+#include <assert.h>
+#include <stdio.h>
 #include "demo_lib.h"
 
-int main()
+void
+main(
+    int argc,
+    char** argv
+    )
 {
-    // TODO: test the demo_lib
-    return 0;
+    // the first arg is the executable name so there's always at least 1.
+    assert(argc >= 1);
+    printf("Testing... %s\n", argv[0]);
 }
