fa008dd17aa81741d8bf852aac99a99feccd7044
Scott Munro <scottnmunro@gmail.com>
2020-03-07 23:18:21 -0800

    holy shit I'm looking at data from a c lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::os::raw::c_char;
 
+#[derive(Debug)]
 #[repr(C)]
 enum FFI_DL_EventType
 {
@@ -28,6 +29,7 @@ struct FFI_DL_DestroyedEvent
     destroyedByte: u8,
 }
 
+#[derive(Debug)]
 #[repr(C)]
 enum FFI_ChangedState
 {
@@ -41,7 +43,7 @@ struct FFI_DL_ChangedEvent
     changedState: FFI_ChangedState,
 }
 
-type FFI_DL_EventList = *const *const FFI_DL_EventType;
+type FFI_DL_EventList = *const *const FFI_DL_Event;
 
 #[link(name = "demo_lib_static", kind = "static")]
 extern {
@@ -53,10 +55,15 @@ fn main() {
     unsafe {
         let mut count: u32 = 0;
         let count_ptr: *mut u32 = &mut count;
-        let mut events: FFI_DL_EventList = std::ptr::null();
-        let events_ptr: *mut FFI_DL_EventList = &mut events;
+        let mut events_buffer: FFI_DL_EventList = std::ptr::null();
+        let events_ptr: *mut FFI_DL_EventList = &mut events_buffer;
         let res = GetEvents(count_ptr, events_ptr);
-        println!("res={}, count={}, events={:?}", res, count, events);
+        println!("res={}, count={}, events={:?}", res, count, events_buffer);
+
+        let events = std::slice::from_raw_parts(events_buffer, count as usize);
+        for event in events {
+            println!("Processing event of type {:?}", (**event).event_type);
+        }
     }
     println!("Done!");
 }
