03594f914f43cd30709f1b563db205d47de2f8bc
Scott Munro <scottnmunro@gmail.com>
2020-03-07 22:47:24 -0800

    ffi linking against (but not using) static lib. wuuuuuutttt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[repr(C)]
+enum FFI_DL_EventType
+{
+    Created,
+    Destroyed,
+    Changed,
+}
+
+#[repr(C)]
+struct FFI_DL_Event
+{
+    event_type: FFI_DL_EventType,
+}
+
+// TODO: add rest of FFI definitions
+
+type FFI_DL_EventList = *const *const FFI_DL_EventType;
+
+#[link(name = "demo_lib_static", kind = "static")]
+extern {
+    fn demo_lib_get_events_ffi(/* Out */ count: *mut u32, /* Out */ events: *mut FFI_DL_EventList) -> i32;
+}
+
 fn main() {
     println!("Hello, world!");
 }
