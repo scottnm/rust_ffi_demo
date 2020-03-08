b9d82c3ca47bf9b01d3cee5a7fc9a4a28cc37e80
Scott Munro <scottnmunro@gmail.com>
2020-03-07 23:00:15 -0800

    woah. I'm calling a c function from rust

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,18 @@ type FFI_DL_EventList = *const *const FFI_DL_EventType;
 
 #[link(name = "demo_lib_static", kind = "static")]
 extern {
-    fn demo_lib_get_events_ffi(/* Out */ count: *mut u32, /* Out */ events: *mut FFI_DL_EventList) -> i32;
+    // TODO: rename this test api to be better
+    fn GetEvents(/* Out */ count: *mut u32, /* Out */ events: *mut FFI_DL_EventList) -> i32;
 }
 
 fn main() {
+    unsafe {
+        let mut count: u32 = 0;
+        let count_ptr: *mut u32 = &mut count;
+        let mut events: FFI_DL_EventList = std::ptr::null();
+        let events_ptr: *mut FFI_DL_EventList = &mut events;
+        let res = GetEvents(count_ptr, events_ptr);
+        println!("res={}, count={}, events={:?}", res, count, events);
+    }
     println!("Hello, world!");
 }
