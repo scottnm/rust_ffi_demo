62fe6fe5b526b7c0f00039c059658551b068d0ca
Scott Munro <scottnmunro@gmail.com>
2020-03-07 18:52:02 -0800

    call first 'failing' API

--- src/demo_lib_test.c
+++ src/demo_lib_test.c
@@ -1,6 +1,18 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "demo_lib.h"
+
+#define EXIT_IF_ERR(err) \
+    do \
+    { \
+        int scoped_err = (err); \
+        if (scoped_err != 0) \
+        { \
+            printf("...Failed! line %i!\n", __LINE__); \
+            exit(scoped_err); \
+        } \
+    } while(0)
 
 void
 main(
@@ -8,7 +20,18 @@ main(
     char** argv
     )
 {
+    int err = 0;
+
     // the first arg is the executable name so there's always at least 1.
     assert(argc >= 1);
     printf("Testing... %s\n", argv[0]);
+
+    uint32_t eventCount;
+    DL_Event_List events;
+
+    err = GetEvents(&eventCount, &events);
+    EXIT_IF_ERR(err);
+    // TODO: call rest of APIs
+
+    printf("...Succeeded!\n");
 }
