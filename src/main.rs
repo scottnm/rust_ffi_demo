1b3b535ffca17f480597804d129ee2c9640cc222
Scott Munro <scottnmunro@gmail.com>
2020-03-13 23:30:13 -0700

    wrote a match to demonstrate exhaustive enum handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod demo_lib;
+use demo_lib::DlEvent;
 
 fn main() {
     let events = demo_lib::get_events();
     println!("count={}, events={:?}", events.len(), events.as_ptr());
     for event in events {
         println!("Processing event of type {:?}", event);
+        match event {
+            DlEvent::CreatedEvent { creationString } => println!("Doing nothing with {:?}", creationString),
+            DlEvent::DestroyedEvent { destroyedByte } => println!("Doing nothing with {:?}", destroyedByte),
+            DlEvent::ChangedEvent  { changedState } => println!("Doing nothing with {:?}", changedState),
+        }
     }
     println!("Done!");
 }
