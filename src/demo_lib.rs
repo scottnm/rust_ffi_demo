93467620c50f86e343aaf269ed332f90d2462130
Scott Munro <scottnmunro@gmail.com>
2020-03-13 23:15:31 -0700

    provided stub implementation of wrapping the FFI_DL_Event in a more rust-like type

--- src/demo_lib.rs
+++ src/demo_lib.rs
@@ -1,48 +1,45 @@
 use std::os::raw::c_char;
 
-// TODO: instead of publicizing all of these c structures, is there
-// any way to get a "rust"ier safe view of the structs
-
 // TODO: don't just make every field public
 
 #[derive(Debug)]
 #[repr(C)]
-pub enum FFI_DL_EventType {
+enum FFI_DL_EventType {
     Created,
     Destroyed,
     Changed,
 }
 
 #[repr(C)]
-pub struct FFI_DL_Event {
+struct FFI_DL_Event {
     pub event_type: FFI_DL_EventType,
 }
 
 #[repr(C)]
-pub struct FFI_DL_CreatedEvent {
+struct FFI_DL_CreatedEvent {
     pub baseEvent: FFI_DL_Event,
     pub creationString: *const c_char,
 }
 
 #[repr(C)]
-pub struct FFI_DL_DestroyedEvent {
+struct FFI_DL_DestroyedEvent {
     pub baseEvent: FFI_DL_Event,
     pub destroyedByte: u8,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub enum FFI_ChangedState {
     A,
     B,
 }
 
-pub struct FFI_DL_ChangedEvent {
+struct FFI_DL_ChangedEvent {
     pub baseEvent: FFI_DL_Event,
     pub changedState: FFI_ChangedState,
 }
 
-pub type FFI_DL_EventList = *const *const FFI_DL_Event;
+type FFI_DL_EventList = *const *const FFI_DL_Event;
 
 #[link(name = "demo_lib_static", kind = "static")]
 extern "C" {
@@ -52,10 +49,24 @@ extern "C" {
     ) -> i32;
 }
 
+// The FFI_ChangedState enum is fine to present in our safe interface, but let's alias away that
+// gnarly FFI name
+pub type ChangedState = FFI_ChangedState;
+
+// FIXME: is there anyway to just present a rust-safe view of things without copying? I feel like
+// this is a job for slices but idk
+#[derive(Debug)]
+pub enum DlEvent {
+    // FIXME: avoid a string allocation+copy
+    CreatedEvent { creationString: String },
+    DestroyedEvent { destroyedByte: u8 },
+    ChangedEvent  { changedState: ChangedState },
+}
+
 // FIXME: maybe there should be some wrapping structure that on drop(..) calls ReturnEvents or
 // something. Then the lifetime of the events could be tied to the lifetime of the structure which
 // would be more semantically correct.
-pub fn get_events() -> Vec<&'static FFI_DL_Event> {
+pub fn get_events() -> Vec<DlEvent> {
     unsafe {
         let mut count: u32 = 0;
         let count_ptr: *mut u32 = &mut count;
@@ -68,9 +79,27 @@ pub fn get_events() -> Vec<&'static FFI_DL_Event> {
 
         // FIXME: blech an allocation on every get_events call? what can I do to get rid of this?
         let mut safe_events = Vec::new();
-        for event in std::slice::from_raw_parts(events_buffer, count as usize) {
+        for event_ptr in std::slice::from_raw_parts(events_buffer, count as usize) {
             // Our lib should never return a nullptr so the unwrap matches the API guarantee
-            safe_events.push(event.as_ref().unwrap())
+            let event = event_ptr.as_ref().unwrap();
+            let safe_event = match event.event_type {
+                /*
+                FFI_DL_EventType::Created => {
+                    DlEvent::Created { creationString: }
+                },
+                FFI_DL_EventType::Destroyed => {
+                    DlEvent::Destroyed { destroyedByte: }
+                },
+                */
+                FFI_DL_EventType::Changed => {
+                    let changedState = std::mem::transmute::<&FFI_DL_Event, &FFI_DL_ChangedEvent>(event).changedState;
+                    DlEvent::ChangedEvent { changedState }
+                },
+                // FIXME: stub
+                _ => DlEvent::DestroyedEvent { destroyedByte: 0 }
+            };
+
+            safe_events.push(safe_event)
         }
         safe_events
     }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let events = demo_lib::get_events();
     println!("count={}, events={:?}", events.len(), events.as_ptr());
     for event in events {
-        println!("Processing event of type {:?}", event.event_type);
+        println!("Processing event of type {:?}", event);
     }
     println!("Done!");
 }
