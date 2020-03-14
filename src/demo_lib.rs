1ec42504d6a57f9a17350008375bb6d7026228c5
Scott Munro <scottnmunro@gmail.com>
2020-03-13 22:51:33 -0700

    we now have a safe (if inefficient wrapper)

--- src/demo_lib.rs
+++ src/demo_lib.rs
@@ -46,14 +46,16 @@ pub type FFI_DL_EventList = *const *const FFI_DL_Event;
 
 #[link(name = "demo_lib_static", kind = "static")]
 extern "C" {
-    // TODO: rename this test api to more clearly look like an external api
     fn DL_GetEvents(
         /* Out */ count: *mut u32,
         /* Out */ events: *mut FFI_DL_EventList,
     ) -> i32;
 }
 
-pub fn get_events() -> &'static [*const FFI_DL_Event] {
+// FIXME: maybe there should be some wrapping structure that on drop(..) calls ReturnEvents or
+// something. Then the lifetime of the events could be tied to the lifetime of the structure which
+// would be more semantically correct.
+pub fn get_events() -> Vec<&'static FFI_DL_Event> {
     unsafe {
         let mut count: u32 = 0;
         let count_ptr: *mut u32 = &mut count;
@@ -63,6 +65,13 @@ pub fn get_events() -> &'static [*const FFI_DL_Event] {
         if res != 0 {
             panic!("DL_GetEvents failed! res={}", res);
         }
-        std::slice::from_raw_parts(events_buffer, count as usize)
+
+        // FIXME: blech an allocation on every get_events call? what can I do to get rid of this?
+        let mut safe_events = Vec::new();
+        for event in std::slice::from_raw_parts(events_buffer, count as usize) {
+            // Our lib should never return a nullptr so the unwrap matches the API guarantee
+            safe_events.push(event.as_ref().unwrap())
+        }
+        safe_events
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ fn main() {
     let events = demo_lib::get_events();
     println!("count={}, events={:?}", events.len(), events.as_ptr());
     for event in events {
-        // FIXME: eliminate this unsafe block wrapping
-        unsafe {
-            println!("Processing event of type {:?}", (**event).event_type);
-        }
+        println!("Processing event of type {:?}", event.event_type);
     }
     println!("Done!");
 }
