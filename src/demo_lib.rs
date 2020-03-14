ab6d35d7075fa52f701f3e9a730e3fe7487d6f9c
Scott Munro <scottnmunro@gmail.com>
2020-03-13 22:29:17 -0700

    moved demo_lib to it's own file

---
+++ src/demo_lib.rs
@@ -0,0 +1,54 @@
+use std::os::raw::c_char;
+
+// TODO: instead of publicizing all of these c structures, is there
+// any way to get a "rust"ier safe view of the structs
+
+// TODO: don't just make every field public
+
+#[derive(Debug)]
+#[repr(C)]
+pub enum FFI_DL_EventType {
+    Created,
+    Destroyed,
+    Changed,
+}
+
+#[repr(C)]
+pub struct FFI_DL_Event {
+    pub event_type: FFI_DL_EventType,
+}
+
+#[repr(C)]
+pub struct FFI_DL_CreatedEvent {
+    pub baseEvent: FFI_DL_Event,
+    pub creationString: *const c_char,
+}
+
+#[repr(C)]
+pub struct FFI_DL_DestroyedEvent {
+    pub baseEvent: FFI_DL_Event,
+    pub destroyedByte: u8,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub enum FFI_ChangedState {
+    A,
+    B,
+}
+
+pub struct FFI_DL_ChangedEvent {
+    pub baseEvent: FFI_DL_Event,
+    pub changedState: FFI_ChangedState,
+}
+
+pub type FFI_DL_EventList = *const *const FFI_DL_Event;
+
+#[link(name = "demo_lib_static", kind = "static")]
+extern "C" {
+    // TODO: rename this test api to more clearly look like an external api
+    pub fn GetEvents(
+        /* Out */ count: *mut u32,
+        /* Out */ events: *mut FFI_DL_EventList,
+    ) -> i32;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,5 @@
-use std::os::raw::c_char;
-
-#[derive(Debug)]
-#[repr(C)]
-enum FFI_DL_EventType {
-    Created,
-    Destroyed,
-    Changed,
-}
-
-#[repr(C)]
-struct FFI_DL_Event {
-    event_type: FFI_DL_EventType,
-}
-
-#[repr(C)]
-struct FFI_DL_CreatedEvent {
-    baseEvent: FFI_DL_Event,
-    creationString: *const c_char,
-}
-
-#[repr(C)]
-struct FFI_DL_DestroyedEvent {
-    baseEvent: FFI_DL_Event,
-    destroyedByte: u8,
-}
-
-#[derive(Debug)]
-#[repr(C)]
-enum FFI_ChangedState {
-    A,
-    B,
-}
-
-struct FFI_DL_ChangedEvent {
-    baseEvent: FFI_DL_Event,
-    changedState: FFI_ChangedState,
-}
-
-type FFI_DL_EventList = *const *const FFI_DL_Event;
-
-#[link(name = "demo_lib_static", kind = "static")]
-extern "C" {
-    // TODO: rename this test api to more clearly look like an external api
-    fn GetEvents(
-        /* Out */ count: *mut u32,
-        /* Out */ events: *mut FFI_DL_EventList,
-    ) -> i32;
-}
+mod demo_lib;
+use demo_lib::*;
 
 fn main() {
     unsafe {
